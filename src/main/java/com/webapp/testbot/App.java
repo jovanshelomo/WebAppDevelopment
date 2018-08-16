e12785eff03b3dd4e5379c548cb0d6fea8e3484a
jovanshelomo <jovanshelomo@gmail.com>
2018-08-16 19:11:09 +0700

    Add files via upload

---
+++ src/main/java/com/webapp/testbot/App.java
@@ -0,0 +1,48 @@
+package com.webapp.testbot;
+
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+import java.util.Timer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends ListenerAdapter
+{
+    public static void main( String[] args ) throws Exception
+    {
+        JDA jda = new JDABuilder(AccountType.BOT).setToken(Ref.token).buildAsync();
+        jda.addEventListener(new App());
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent evt)
+    {
+        //objects
+        User objUsers = evt.getAuthor();
+        MessageChannel objMsgChannel = evt.getChannel();
+        Message objMsg = evt.getMessage();
+
+
+        //commands
+        if(objMsg.getContentRaw().equalsIgnoreCase(Ref.prefix + "Tes"))
+        {
+            objMsgChannel.sendMessage(objUsers.getAsMention()+" HALO "+objUsers.getName()).queue();
+            objMsgChannel.
+
+
+
+        }
+
+
+    }
+
+}
---
+++ src/main/java/com/webapp/testbot/Ref.java
@@ -0,0 +1,6 @@
+package com.webapp.testbot;
+
+public class Ref {
+    public static String token = "NDc5NjA5OTI2NzcyMTk1MzI5.Dlbv-w.ieQwmQdDVHIMG5E5d15NOtmPfTY";
+    public static String prefix = "!";
+}
