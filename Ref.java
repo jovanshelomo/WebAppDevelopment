e4b48a640120df39f0dd3dd620a5e1bb42a3cb50
jovanshelomo <jovanshelomo@gmail.com>
2018-08-17 23:22:14 +0700

    Add files via upload

---
+++ Ref.java
@@ -0,0 +1,11 @@
+package com.webapp.testbot;
+
+public class Ref {
+    public static String token = "NDc5NjA5OTI2NzcyMTk1MzI5.Dlbv-w.ieQwmQdDVHIMG5E5d15NOtmPfTY";
+    public static String prefix = "!";
+    public static String info =
+            "!changenick or !cn --> `!changenick @mention <name> <class>`" +
+            "\n!halo" +
+            "\n!count --> `!count <number>`" +
+            "\n!info --> `!info` or `!info @mention`";
+}
